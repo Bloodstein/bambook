1a4018de0311adfa065bf99753e9c9477dd99e59
ziker99 <saom_pashin@mail.ru>
2017-10-12 11:38:17 +0500

    ReadyToCSS

--- landing/models.py
+++ landing/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Subscriber(models.Model):
 	email = models.EmailField()
 	name = models.CharField(max_length=30)
+
+	def __str__(self):
+		return '%s %s' % (self.name, self.email)
--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .forms import SubscriberForm
 
 def landing(request):
+	form = SubscriberForm(request.POST or None)
+	
+	if request.method == 'POST' and form.is_valid():
+		data = form.cleaned_data
+		print(data)
+		new_form = form.save()
 	return render(request, 'landing/landing.html', locals())
