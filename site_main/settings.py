0eb0c6923b70680d119b9456b939e528aee2d7e9
student43 <studkab43@jdk.ru>
2017-10-21 08:36:25 +0500

    UpdateFolderStatic

--- site_main/settings.py
+++ site_main/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'site_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/Users/expert/bambook/templates/'],
+        'DIRS': ['./templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), '/bambook/static/')
-MEDIA_ROOT = '/bambook/static/media/'
+STATIC_URL = './static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), './static/')
+MEDIA_ROOT = './static/media/'
